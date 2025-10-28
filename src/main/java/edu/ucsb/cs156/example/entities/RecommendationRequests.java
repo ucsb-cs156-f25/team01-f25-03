9e03b4f1ad43b69e25bd4d9547a668dc57b072a4
tarang-netizen <tarang@ucsb.edu>
2025-10-28 13:31:46 -0700

    Add RecommendationRequests entity class

---
+++ src/main/java/edu/ucsb/cs156/example/entities/RecommendationRequests.java
@@ -0,0 +1,23 @@
+package edu.ucsb.cs156.example.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "recommendation_requests")
+public class RecommendationRequests {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String requesterEmail;
+    private String professorEmail;
+    private String explanation;
+    private LocalDateTime dateRequested;
+    private LocalDateTime dateNeeded;
+    private boolean
