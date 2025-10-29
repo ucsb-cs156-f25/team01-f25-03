ecdfb94355b61e04ac50b3135b1905a99928d5c3
Joshua Chu <chujoshua.2004@gmail.com>
2025-10-28 17:29:48 -0700

    updated get single method

--- src/main/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationController.java
@@ -1,6 +1,7 @@
 package edu.ucsb.cs156.example.controllers;
 
 import edu.ucsb.cs156.example.entities.UCSBOrganization;
+import edu.ucsb.cs156.example.errors.EntityNotFoundException;
 import edu.ucsb.cs156.example.repositories.UCSBOrganizationRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
