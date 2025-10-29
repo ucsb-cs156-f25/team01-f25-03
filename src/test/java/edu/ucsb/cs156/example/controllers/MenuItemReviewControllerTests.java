24fa43205a41c141d8914a7046ed41e96eafce77
Adam Wang <adamwang@ucsb.edu>
2025-10-28 23:06:19 -0700

    Implement PUT API route

--- src/test/java/edu/ucsb/cs156/example/controllers/MenuItemReviewControllerTests.java
+++ src/test/java/edu/ucsb/cs156/example/controllers/MenuItemReviewControllerTests.java
@@ -8,6 +8,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import edu.ucsb.cs156.example.ControllerTestCase;
@@ -24,6 +25,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -57,6 +59,28 @@ public class MenuItemReviewControllerTests extends ControllerTestCase {
   @Test
   public void test_cant_get_single_without_login() throws Exception {
     mockMvc.perform(get("/api/menuitemreview?id=1")).andExpect(status().is(403));
+  }
+
+  @WithMockUser(roles = {})
+  @Test
+  public void test_cant_put_without_login() throws Exception {
+    mockMvc
+        .perform(
+            put(
+                "/api/menuitemreview?itemId=1&reviewerEmail=abc@xyz.com&stars=1"
+                    + "dateReviewed=2025-10-28T00:00:00&comments=terrible"))
+        .andExpect(status().is(403));
+  }
+
+  @WithMockUser(roles = {"USER"})
+  @Test
+  public void test_cant_put_as_user() throws Exception {
+    mockMvc
+        .perform(
+            put(
+                "/api/menuitemreview?itemId=1&reviewerEmail=abc@xyz.com&stars=1"
+                    + "dateReviewed=2025-10-28T00:00:00&comments=terrible"))
+        .andExpect(status().is(403));
   }
 
   // Tests with mocks for database actions
@@ -181,5 +205,91 @@ public class MenuItemReviewControllerTests extends ControllerTestCase {
     Map<String, Object> json = responseToJson(response);
     assertEquals("EntityNotFoundException", json.get("type"));
     assertEquals("MenuItemReview with id 7 not found", json.get("message"));
+  }
+
+  @WithMockUser(roles = {"ADMIN", "USER"})
+  @Test
+  public void admin_can_edit_existing_menuitemreview() throws Exception {
+    // arrange
+    LocalDateTime ldt1 = LocalDateTime.parse("2025-10-28T00:00:00");
+    LocalDateTime ldt2 = LocalDateTime.parse("2025-10-29T00:00:00");
+
+    MenuItemReview menuItemReviewOrig =
+        MenuItemReview.builder()
+            .itemId(Long.valueOf(1))
+            .reviewerEmail("johnsmith@gmail.com")
+            .stars(3)
+            .dateReviewed(ldt1)
+            .comments("mid")
+            .build();
+
+    MenuItemReview menuItemReviewEdited =
+        MenuItemReview.builder()
+            .itemId(Long.valueOf(2))
+            .reviewerEmail("jackdoe@yahoo.com")
+            .stars(5)
+            .dateReviewed(ldt2)
+            .comments("amazing")
+            .build();
+
+    String requestBody = mapper.writeValueAsString(menuItemReviewEdited);
+
+    when(menuItemReviewRepository.findById(eq(67L))).thenReturn(Optional.of(menuItemReviewOrig));
+
+    // act
+    MvcResult response =
+        mockMvc
+            .perform(
+                put("/api/menuitemreview?id=67")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .characterEncoding("utf-8")
+                    .content(requestBody)
+                    .with(csrf()))
+            .andExpect(status().isOk())
+            .andReturn();
+
+    // assert
+    verify(menuItemReviewRepository, times(1)).findById(67L);
+    verify(menuItemReviewRepository, times(1))
+        .save(menuItemReviewEdited); // should be saved with correct user
+    String responseString = response.getResponse().getContentAsString();
+    assertEquals(requestBody, responseString);
+  }
+
+  @WithMockUser(roles = {"ADMIN", "USER"})
+  @Test
+  public void admin_cannot_edit_menuitemreview_that_does_not_exist() throws Exception {
+    // arrange
+    LocalDateTime ldt1 = LocalDateTime.parse("2025-10-28T00:00:00");
+
+    MenuItemReview menuItemReviewEdited =
+        MenuItemReview.builder()
+            .itemId(Long.valueOf(1))
+            .reviewerEmail("johnsmith@gmail.com")
+            .stars(3)
+            .dateReviewed(ldt1)
+            .comments("mid")
+            .build();
+
+    String requestBody = mapper.writeValueAsString(menuItemReviewEdited);
+
+    when(menuItemReviewRepository.findById(eq(67L))).thenReturn(Optional.empty());
+
+    // act
+    MvcResult response =
+        mockMvc
+            .perform(
+                put("/api/menuitemreview?id=67")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .characterEncoding("utf-8")
+                    .content(requestBody)
+                    .with(csrf()))
+            .andExpect(status().isNotFound())
+            .andReturn();
+
+    // assert
+    verify(menuItemReviewRepository, times(1)).findById(67L);
+    Map<String, Object> json = responseToJson(response);
+    assertEquals("MenuItemReview with id 67 not found", json.get("message"));
   }
 }
